aa8317872213a3f7e56bf65d43443d8d91ad428f
lidongshuai <756334773@qq.com>
2023-02-21 09:59:28 +0800

    new version

---
+++ src/views/APPsearch.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="container">
+		我是APPsearch
+	</div>
+</template>
+
+<script setup lang="ts" name="APPsearch"></script>
+
+<style></style>
---
+++ src/views/APPstat.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="container">
+		我是APPstat
+	</div>
+</template>
+
+<script setup lang="ts" name="APPstat"></script>
+
+<style></style>
